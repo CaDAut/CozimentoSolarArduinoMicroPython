bdfd56326702ba5e51c26edc2da72d4f9b8cbce6
CaDAut <136136768+CaDAut@users.noreply.github.com>
2023-06-12 13:53:20 -0300

    Update controlamotor_passo.py

--- controlamotor_passo.py
+++ controlamotor_passo.py
@@ -4,12 +4,12 @@
 #include <WebServer.h>
 #include <AccelStepper.h>
 
-const char* ssid = "NOME_DA_REDE_WIFI";
-const char* password = "SENHA_DA_REDE_WIFI";
+const char* ssid = "NOME_DA_SUA_REDE_WIFI";
+const char* password = "SENHA_DA_SUA_REDE_WIFI";
 
 WebServer server(80);
 
-// Configurações dos motores de passo
+#Configurações dos motores de passo
 #define MOTOR1_STEP_PIN 26
 #define MOTOR1_DIR_PIN 27
 #define MOTOR2_STEP_PIN 32
@@ -28,7 +28,7 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  // Conectar ao Wi-Fi
+  #Conectar ao Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -39,12 +39,12 @@ void setup() {
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
 
-  // Configurar roteamento da Web
+  #Configurar roteamento da Web
   server.on("/", handleRoot);
   server.onNotFound(handleNotFound);
   server.begin();
 
-  // Configurar motores de passo
+  #Configurar motores de passo
   motor1.setMaxSpeed(1000);
   motor1.setAcceleration(100);
   motor2.setMaxSpeed(1000);
@@ -58,23 +58,23 @@ void setup() {
 void loop() {
   server.handleClient();
 
-  // Verificar se há novas coordenadas a cada 5 minutos
+  #Verificar se há novas coordenadas a cada 5 minutos
   if (millis() % (5 * 60 * 1000) == 0) {
-    // Obter as coordenadas do usuário externo
-    // (você precisará implementar esse código)
+    """Obter as coordenadas do usuário externo
+    (implementar código para obtenção das coordenadas externas)"""
     float coord1 = getCoordinate(1);
     float coord2 = getCoordinate(2);
     float coord3 = getCoordinate(3);
     float coord4 = getCoordinate(4);
 
-    // Mover os motores para as coordenadas fornecidas
+    #Mover os motores para as coordenadas fornecidas
     motor1.moveTo(coord1);
     motor2.moveTo(coord2);
     motor3.moveTo(coord3);
     motor4.moveTo(coord4);
   }
 
-  // Atualizar os motores
+  #Atualizar os motores
   motor1.run();
   motor2.run();
   motor3.run();
@@ -92,11 +92,11 @@ void handleNotFound() {
 }
 
 float getCoordinate(int motorIndex) {
-  // Aqui você precisa implementar a lógica para obter as coordenadas
-  // do usuário externo para cada motor. Pode ser por meio de uma
-  // comunicação serial, conexão com banco de dados ou qualquer outra
-  // forma de obter os valores.
-  // Neste exemplo, simplesmente retornamos um valor fixo.
+  """Implementar a lógica para obter as coordenadas
+  do usuário externo para cada motor. Provavelmente será por meio de uma
+  comunicação serial ou conexão com banco de dados via internet.
+  A tendência é de que utilizemos a conexão com BD via internet
+  aproveitando os scripts calcula_posSol.py e o pos_insereData.py"""
   switch (motorIndex) {
     case 1:
       return 1000.0;
@@ -110,3 +110,41 @@ float getCoordinate(int motorIndex) {
       return 0.0;
   }
 }
+
+"""A biblioteca AccelStepper é uma biblioteca muito útil para controlar motores de passo 
+com Arduino e plataformas compatíveis, como o ESP32. 
+Ela fornece uma maneira conveniente de controlar a velocidade e a aceleração dos 
+motores de passo, permitindo movimentos suaves e precisos.
+A biblioteca AccelStepper oferece diferentes modos de operação para controlar os 
+motores de passo, como "passo total" (full step), "meio passo" (half step) e 
+"passo de micro" (microstepping). Ela também suporta aceleração e desaceleração suave 
+dos motores, o que é importante para evitar chocar os motores em altas 
+velocidades e obter um movimento mais preciso.
+
+Alguns conceitos importantes e funções-chave da biblioteca AccelStepper:
+
+Passos e velocidade: A biblioteca trabalha com base em passos. 
+Cada motor de passo tem uma certa resolução, o que significa que ele se move em 
+incrementos discretos. Por exemplo, se um motor de passo tem uma resolução de 200 passos por 
+rotação completa, ele se moverá 1/200 de volta em cada passo. 
+A velocidade é especificada em passos por segundo ou passos por minuto.
+
+Aceleração: A biblioteca permite definir uma aceleração, ou seja, a taxa de mudança da 
+velocidade. Isso permite que o motor de passo atinja a velocidade desejada gradualmente, 
+evitando chocá-lo com aceleração instantânea. A aceleração é especificada em passos por 
+segundo ao quadrado ou passos por minuto ao quadrado.
+
+Métodos principais: A AccelStepper oferece métodos principais para controlar os motores 
+de passo, como moveTo(), move(), run(), setSpeed(), setMaxSpeed(), setAcceleration(), 
+isRunning(), entre outros. Esses métodos são usados para definir a posição de destino, 
+mover o motor, atualizar o movimento e configurar a velocidade e aceleração.
+
+Modos de operação: A biblioteca AccelStepper suporta diferentes modos de operação, 
+como "passo total" (onde o motor é controlado por sinais de pulso e direção), 
+"meio passo" (onde o motor pode se mover em incrementos menores do que um passo completo) 
+e "passo de micro" (que permite ainda mais microstepping, proporcionando movimentos mais suaves).
+
+Esses são apenas alguns conceitos e funcionalidades básicas da biblioteca AccelStepper. 
+Ela oferece muitas outras opções de configuração e controle avançado de motores de passo. 
+Se você precisar de informações mais detalhadas sobre a biblioteca, 
+sugiro consultar a documentação oficial da AccelStepper."""
